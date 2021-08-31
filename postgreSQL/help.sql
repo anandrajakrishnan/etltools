5a57e55c2a2f0039270af88cfadbe7b7da4e1ba8
Anand Rajakrishnan <48420816+anandrajakrishnan@users.noreply.github.com>
2021-08-31 13:48:26 -0400

    Update help.sql

--- postgreSQL/help.sql
+++ postgreSQL/help.sql
@@ -4,3 +4,14 @@ FROM pg_stat_activity as p
 WHERE query != '<IDLE>' AND query NOT ILIKE '%pg_stat_activity%' 
 --and query like 'COPY %'
 ;
+--get column metadata
+SELECT *
+FROM information_schema.columns
+where table_schema in ('xxxxx')
+and table_name='xxxxxxxx'
+;
+--Get table size
+select pg_size_pretty(pg_relation_size('schema.table'));
+SELECT split_part(pg_size_pretty(pg_relation_size('schema.table')),' ',1)::real/1024;
+--Get version of DB
+select current_database();
