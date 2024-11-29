36fab92595245219f194e8f43234380469877bee
Anand Rajakrishnan <48420816+anandrajakrishnan@users.noreply.github.com>
2024-11-29 14:12:02 -0500

    Update help.sh

--- awsCLI/help.sh
+++ awsCLI/help.sh
@@ -18,3 +18,21 @@ aws s3api delete-object --bucket vnsny-das-staging-test --key tmg_claims_diagnos
 #check size of s3 bucket
 
 aws s3 ls s3://vnsny-das-staging-test --recursive --human-readable --summarize --profile test
+
+# configure aws cli on laptop
+
+aws configure sso
+
+# Get list of all ec2 instances in table format
+
+aws ec2 describe-instances --filters Name=tag-key,Values=Name --query "Reservations[*].Instances[*].{Instance:InstanceId,State:State.Name,Name:Tags[?Key=='Name']|[0].Value}" --output table --region "ca-central-1"
+
+# login to an ec2 instance
+aws ssm start-session --target i-xxxxxxxxxxxxxxxx --region "ca-central-1"
+
+# Access aws secret
+# parameters:
+#    secret-id : name of aws secret
+#    query : name of the attribute from aws secret. usually it will be SecretString
+#    jq -r : within single quote use dot operator to access each key in the json
+aws secretsmanager get-secret-value --secret-id secretName --query SecretString --output text | jq -r '.passphrase'
