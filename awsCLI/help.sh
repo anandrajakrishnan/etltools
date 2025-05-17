038fb95cc310ac09edb88e9e9fe064350706cd94
anandrajakrishnan <anand.rajakrishnan@gmail.com>
2025-05-17 09:57:38 -0400

    removed name reference to organization

--- awsCLI/help.sh
+++ awsCLI/help.sh
@@ -5,19 +5,19 @@ aws s3 ls
 aws s3 ls --profile test
 
 #ls on a specific bucket
-aws s3 ls vnsny-das-staging-test/ --profile test
+aws s3 ls xxxx-das-staging-test/ --profile test
 
 #copy a file from local to s3 bucket
 
-aws s3 cp C:\Documents\dataFiles\VNS_CBH_CMGC_DATA_EXTRACTS_CDC1_2021-07-06.zip s3://vnsny-das-staging-test/ --profile test
+aws s3 cp C:\Documents\dataFiles\VNS_CBH_CMGC_DATA_EXTRACTS_CDC1_2021-07-06.zip s3://xxxx-das-staging-test/ --profile test
 
 #delete a file from s3 bucket
 
-aws s3api delete-object --bucket vnsny-das-staging-test --key tmg_claims_diagnosis.csv --profile test
+aws s3api delete-object --bucket xxxx-das-staging-test --key tmg_claims_diagnosis.csv --profile test
 
 #check size of s3 bucket
 
-aws s3 ls s3://vnsny-das-staging-test --recursive --human-readable --summarize --profile test
+aws s3 ls s3://xxxx-das-staging-test --recursive --human-readable --summarize --profile test
 
 # configure aws cli on laptop
 
